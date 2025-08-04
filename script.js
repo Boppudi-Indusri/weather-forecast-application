86d6b1c10447d8316b8eb8e6a89b4c7a62b7b98f
boppudi-indusri <boppudiindusri681@gmail.com>
2025-08-04 13:54:15 +0530

    Adding accessing ids

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 const API_KEY = "3d274ddf1782a01217dbc77e83763990"; // Replace with your OpenWeatherMap API key
+const cityInput = document.getElementById("cityInput");
+const searchBtn = document.getElementById("searchBtn");
+const locationBtn = document.getElementById("locationBtn");
+const weatherInfo = document.getElementById("weatherInfo");
+const forecastContainer = document.getElementById("forecast");
+const recentCitiesDropdown = document.getElementById("recentCities");
